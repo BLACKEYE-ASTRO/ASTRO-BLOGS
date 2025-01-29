b7bd38f324ef20b072f978e9e81753ca2597f6f7
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2025-01-29 13:51:17 +0530

    All done

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
-require('dotenv').config(); // Load environment variables
-
+require('dotenv').config(); 
 const { Pool } = require('pg');
 
-// Configure PostgreSQL connection using environment variables
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
